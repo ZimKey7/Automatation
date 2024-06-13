3dd1d02b15c5bda1d7af6ce78b9664549b964a6c
Vik <arcovd00@gmail.com>
2024-06-13 20:32:15 +0300

    Refactor

--- Lesson2/lesson_2_task_10.py
+++ Lesson2/lesson_2_task_10.py
@@ -1,8 +1,7 @@
 def bank(X, Y):
-    total_amaunt = 0
+    total_amaunt = X
     for i in range(1, Y+1):
-        total_amaunt += X
         total_amaunt += total_amaunt / 10
     print(total_amaunt)
 
-bank(100, 4)
+bank(1500, 5)
